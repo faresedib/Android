7a8de184a014b0ddc7faa38707aa57350c514048
Fares <fendong@estudiante.edib.es>
2021-12-28 00:38:53 +0100

    Codelabs,Unidad 2,lección 2

--- CodeLabs/Unidad2/Intents_y_Actividades/app/src/main/java/com/example/intentsactivities/SecondActivity.java
+++ CodeLabs/Unidad2/Intents_y_Actividades/app/src/main/java/com/example/intentsactivities/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ public class SecondActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "Respuesta";
     private EditText mReply;
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +26,47 @@ public class SecondActivity extends AppCompatActivity {
         textView.setText(message);
     }
 
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
     public void returnReply(View view) {
         String respuesta = mReply.getText().toString();
         Intent intent_origen = new Intent(this, MainActivity.class);
         intent_origen.putExtra(EXTRA_REPLY, respuesta);
         setResult(RESULT_OK,intent_origen);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
