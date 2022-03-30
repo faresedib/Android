7c43dae1bb5cb714ae5bb6939eb426760cac60af
Fares <fendong@estudiante.edib.es>
2022-03-30 19:26:23 +0200

    Conceptos intermedio de kotlin con brais(commit)

---
+++ Kotlin/ConceptosIntermedios(Con Brais)/app/src/main/java/com/example/kotlin/Work.kt
@@ -0,0 +1,7 @@
+package com.example.kotlin
+
+//Una clase abstracta es aquella que no necesita una implementación, sólo define un comportamiento
+abstract class Work {
+
+    abstract fun goToWork()
+}
