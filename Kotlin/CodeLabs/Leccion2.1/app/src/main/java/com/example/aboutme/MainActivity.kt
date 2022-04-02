9e454c65de9663aa3e6d3163a35e57bad27ff15c
Fares <fendong@estudiante.edib.es>
2022-04-03 00:35:31 +0200

    Codelabas Kotlin: Leccion 2.1 completado

---
+++ Kotlin/CodeLabs/Leccion2.1/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.aboutme
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
