4ed315f6bc43214d9f705e5c17943084273b466c
Fares <fendong@estudiante.edib.es>
2022-04-04 20:16:01 +0200

    Codelabas Kotlin: Leccion 2.2 completada

--- Kotlin/CodeLabs/Leccion2.1/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Kotlin/CodeLabs/Leccion2.1/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.aboutme
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBar
 
@@ -12,5 +19,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Añadimos los oyentes a las vistas
+        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener { updateNickname(it) }
+
+    }
+
+    private fun clickHandlerFunction(viewThatIsClicked: View) {
+
+    }
+
+    private fun addNickname(view: View) {
+        val edit_text: EditText = findViewById(R.id.nickname_edit)
+        val text: TextView = findViewById(R.id.nickname_text)
+
+        text.text = edit_text.text.toString()//Establecemos el texto en el TextView
+        edit_text.visibility = View.GONE //Ocultamos el editText
+        view.visibility = View.GONE //Ocultamos el editText
+        text.visibility = View.VISIBLE //Ocultamos el editText
+
+        // Hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+
+    private fun updateNickname(view: View) {
+        val edit_text = findViewById<EditText>(R.id.nickname_edit)
+        val done_button = findViewById<Button>(R.id.done_button)
+
+        edit_text.visibility = View.VISIBLE
+        done_button.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        edit_text.requestFocus()//Establecemos el foco en el editText
+
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(edit_text, 0)
     }
 }
