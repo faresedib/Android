928e567d96d2cb6d819f63c25d860b6f099d3568
Fares <fendong@estudiante.edib.es>
2022-04-07 18:09:16 +0200

    Codelabs Kotlin: Leccion 2.3 desafío1 Completado

--- Kotlin/CodeLabs/Leccion2.3/app/src/main/java/com/example/colomyviews/MainActivity.kt
+++ Kotlin/CodeLabs/Leccion2.3/app/src/main/java/com/example/colomyviews/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.colomyviews
 
 import android.graphics.Color
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
 
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
             // Boxes using Color class colors for the background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_one)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
             else -> view.setBackgroundColor(Color.LTGRAY)
