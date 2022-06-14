9a7448372044337fb5f793a963a3068c99d2e599
fares-developer <edibfares@gmail.com>
2022-06-14 18:42:42 +0200

    Codelabs de Kotlin: Leccion 9.2 WorkManager

--- Kotlin/CodeLabs/Leccion9/DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ Kotlin/CodeLabs/Leccion9/DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,12 +17,26 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.example.android.devbyteviewer.DevByteApplication.Companion.WORK_NAME
+import com.example.android.devbyteviewer.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
  */
 class DevByteApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    companion object {
+        const val WORK_NAME:String = "com.example.android.devbyteviewer.work.RefreshDataWorker"
+    }
 
     /**
      * onCreate is called before the first screen is shown to the user.
@@ -32,6 +46,41 @@ class DevByteApplication : Application() {
      */
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+    }
+
+    /**
+     * Setup WorkManager background job to 'fetch' new network data daily.
+     */
+    private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            //UNMETERED es para que el RequestWork sólo se ejecute en redes sin medidor
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)//Se ejecuta sólo cuando la batería no está baja
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//Version de Android igual o superior a 6
+                    setRequiresDeviceIdle(true)
+                }
+            }
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        Timber.d("Periodic Work request for sync is scheduled")
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
+
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
     }
 }
