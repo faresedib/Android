0482a763c712f33bc2e5a0211334560964d31e91
fares-developer <faresendongpro@gmail.com>
2023-03-30 19:01:51 +0200

    Jetpack: Commit de seguridad

--- Kotlin/Jetpack/2BuildUI/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Kotlin/Jetpack/2BuildUI/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
